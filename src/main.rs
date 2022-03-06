0d1b2089a7542e0b5a554246df9aa9450c117684
Ryan Ruckley <ryan.ruckley@optus.com.au>
2022-03-06 12:39:09 +1100

    Getting closer to compiling

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,19 @@ extern crate redis;
 use amqp::{Basic, Channel, Session};
 use std::error::Error;
 use redis::Commands;
-use redis::Connection;
+//use redis::Connection;
 
+// Get message from AMQP queue and return body
+fn get_message(channel: &Channel,queue_name: String) -> Result<String, Box<dyn Error>> {
+    for delivery in channel.basic_get(&queue_name,false) {
+        
+        let body = String::from_utf8(delivery.body.to_vec())?;
+        //let id = String::from_utf8(delivery.delivery_tag.to_vec())?;
+        return Ok(body);
+    }
+    
+    Ok("striong".to_string())
+}
 fn main() {
     let amqp_url = "amqp://10.122.13.226:5672";
     let redis_url = "redis://10.122.13.226:6379";    
@@ -18,11 +29,12 @@ fn main() {
     
     // Redis Connection
     let redis_client = redis::Client::open(redis_url).unwrap();
-    let mut redis = redis_client.get_connection();
+    let mut redis = redis_client.get_connection().unwrap();
 
     loop {
-        //let (msg_id, msg_body) = get_message(&ch.1, &queue_name).unwrap();
-        //let msg_id = get_message(&ch, queue_name);
-        //redis.set(msg_id, "teste".to_string()).unwrap();    
+        let msg_id = "String".to_string();
+        let msg_body = get_message(&channel, queue_name.to_string()).unwrap();
+        let cmd = redis::Cmd::new().arg("SET").arg(msg_id).arg(msg_body);
+        cmd.query::<()>(&redis).unwrap();
     }
 }
